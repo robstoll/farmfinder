32e67b65a1fb2ffc92e3d786c6e6fd6915655707
Robert Stoll <rstoll@tutteli.ch>
2015-01-13 17:45:14 +0100

    - renamed HomeController to MainController - created home page - created try it out page (not done yet)

--- FarmFinder/SearchApi/Controllers/MainController.cs
+++ FarmFinder/SearchApi/Controllers/MainController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SearchApi.Models;
 
 namespace SearchApi.Controllers
 {
-    public class HomeController : Controller
+    public class MainController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
+        }
+
+        public ActionResult TryIt()
+        {
+            return View(new QueryDto { Radius = 30, Latitude = 48.306940, Longitude = 14.285830 });
         }
     }
 }
